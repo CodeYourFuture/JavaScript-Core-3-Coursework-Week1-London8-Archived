9b69cca1659f8daa2aea3c5833bf07caeab041e9
Ika Khoza <ikaneng.khoza@gmail.com>
2022-02-21 09:34:54 +0000

    Update script.js

--- mandatory/debugging-practice/script.js
+++ mandatory/debugging-practice/script.js
@@ -38,7 +38,7 @@ function submit() {
     return false;
   } else {
     let book = new Book(title.value, title.value, pages.value, check.checked);
-    library.push(book);
+    myLibrary.push(book);
     render();
   }
 }
@@ -54,7 +54,7 @@ function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+  for (let n = rowsNumber - 1; n > 0; n-- ){
     table.deleteRow(n);
   }
   //insert updated row and cells
